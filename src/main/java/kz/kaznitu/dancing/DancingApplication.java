4446279aa90c8e16755fbcddd77b63427531db36
Bope <bope.2798@gmail.com>
2018-05-04 06:29:58 +0600

    commit1

---
+++ src/main/java/kz/kaznitu/dancing/DancingApplication.java
@@ -0,0 +1,12 @@
+package kz.kaznitu.dancing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DancingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DancingApplication.class, args);
+	}
+}
---
+++ src/main/java/kz/kaznitu/dancing/HomeController.java
@@ -0,0 +1,15 @@
+package kz.kaznitu.dancing;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(path = "/")
+
+    public String home (){
+        return "home";
+    }
+
+}
